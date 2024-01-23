ce7ede1378ce373b7fac6e3445551731bcc50d27
caslini.20353 <caslini.20353@E2-9-PC20>
2024-01-23 12:47:26 +0100

    Aggiungere i file di progetto.

---
+++ esinclassecreazionetest/classecomponente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esinclassecreazionetest
+{
+    public abstract class classecomponente
+    {
+        public abstract void Add(classecomponente componente);
+        public abstract void Remove(int indice);
+        public abstract classecomponente GetChild(int indice);
+        public abstract string ToString();
+        public abstract bool Equals(object obj);
+        public abstract int Punteggio();
+    }
+}
